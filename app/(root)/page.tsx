3fee708505a7f76f10ad7d8d90a90c9e629d96fc
Danyal Imran <dan.imran97@gmail.com>
2025-02-06 18:13:17 -0500

    create productlist grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
 //this is the root page of the app
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const Homepage = () => {
-  return <>TheStore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
