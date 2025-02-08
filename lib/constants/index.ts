572648be9abe7af711e838c65ac6825933e4a150
Danyal Imran <dan.imran97@gmail.com>
2025-02-07 19:13:06 -0500

    introduce/install zod for validators and utils

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -3,3 +3,5 @@ export const APP_DESCRIPTION =
   process.env.APP_DESCRIPTION || "A modern e-commerce store";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVERL_URL || "http://localhost:3000";
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
