8d0153e12cb7f40bc6cb4dc51d92084b70291276
Danyal Imran <dan.imran97@gmail.com>
2025-04-01 12:14:24 -0400

    cart having issues with renderin but once fixed cart will render add items to cart

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useRouter } from "next/router";
+import { useSonner } from "sonner";
+import { useTransition } from "react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+
+import { Cart } from "@/types";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const sonner = useSonner();
+
+  const { toast } = useSonner();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:cols-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
